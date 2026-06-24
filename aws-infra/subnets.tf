828865c6efd7c408ba00142422deafd1faec6e32
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-24 21:45:38 +0530

    Created Private subnet without route table assoc

--- aws-infra/subnets.tf
+++ aws-infra/subnets.tf
@@ -3,11 +3,21 @@ resource "aws_subnet" "ManagementSubnet" {
     cidr_block = "10.0.0.0/24"
 
     tags = {
-      "Name" = "Management Subnet"
+      "Name" = "ManagementSubnet"
     }
 }
 
 resource "aws_route_table_association" "ManagementSubnetRTassoc" {
     subnet_id = aws_subnet.ManagementSubnet.id
     route_table_id = aws_route_table.to_igw.id
+}
+
+
+resource "aws_subnet" "PrivateSubnet" {
+    vpc_id = aws_vpc.NatInstanceDemoVPC.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+      "Name" = "PrivateSubnet"
+    }
 }
