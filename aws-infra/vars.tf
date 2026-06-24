32752f9a0fad1375c5d2e5f5aad12e8128b267a2
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-24 21:48:34 +0530

    Added public IP var

--- aws-infra/vars.tf
+++ aws-infra/vars.tf
@@ -3,3 +3,9 @@ variable "region" {
     description = "aws region"
     default = "ap-south-2"
 }
+
+variable "my_public_ip" {
+    type = string
+    description = "my public ip"
+    default = "0.0.0.0/0"
+}
