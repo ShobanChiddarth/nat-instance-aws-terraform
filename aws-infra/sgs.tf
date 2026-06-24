0d5e7a95fb0c6b01a0bf08d5674173b07464731a
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-24 22:15:33 +0530

    Created sg for NAT instance

--- aws-infra/sgs.tf
+++ aws-infra/sgs.tf
@@ -37,3 +37,30 @@ resource "aws_security_group" "PEC2_sg" {
         cidr_blocks = [ "0.0.0.0/0" ]
     }
 }
+
+resource "aws_security_group" "nat_instance_sg" {
+    name = "nat_instance_sg"
+    vpc_id = aws_vpc.NatInstanceDemoVPC.id
+    description = "allow everything"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        security_groups = [ aws_security_group.bastion_sg.id ]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ aws_vpc.NatInstanceDemoVPC.cidr_block ]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
