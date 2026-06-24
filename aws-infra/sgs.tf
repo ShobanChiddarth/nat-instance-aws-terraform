4e7a6f677f02bb464a7f377fdbff17aee1db8795
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-24 22:05:49 +0530

    Added sg for Private EC2

--- aws-infra/sgs.tf
+++ aws-infra/sgs.tf
@@ -17,3 +17,23 @@ resource "aws_security_group" "bastion_sg" {
         cidr_blocks = [ "0.0.0.0/0" ]
     }
 }
+
+resource "aws_security_group" "PEC2_sg" {
+    name = "PEC2_sg"
+    vpc_id = aws_vpc.NatInstanceDemoVPC.id
+    description = "allow ssh from bastion"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        security_groups = [ aws_security_group.bastion_sg.id ]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
