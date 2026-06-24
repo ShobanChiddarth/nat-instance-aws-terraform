64258d0054b5048d81808553dc97b3b692dcee38
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-24 22:23:39 +0530

    Created nat instance route table

--- aws-infra/rts.tf
+++ aws-infra/rts.tf
@@ -12,3 +12,12 @@ resource "aws_route_table" "to_igw" {
         "Name" = "to_igw"
     }
 }
+
+resource "aws_route_table" "to_nat_instance" {
+    vpc_id = aws_vpc.NatInstanceDemoVPC.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        network_interface_id = aws_instance.nat_instance.primary_network_interface_id
+    }
+}
