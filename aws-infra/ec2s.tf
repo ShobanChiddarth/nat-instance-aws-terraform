92741f474adcfa857eebabeecfddf23c7365d663
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-24 22:08:23 +0530

    Created 2 private EC2s

--- aws-infra/ec2s.tf
+++ aws-infra/ec2s.tf
@@ -19,3 +19,30 @@ resource "aws_instance" "bastion" {
     }
 }
 
+resource "aws_instance" "PEC21" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.PrivateSubnet.id
+    associate_public_ip_address = false
+    user_data = local.base_init
+    vpc_security_group_ids = [ aws_security_group.PEC2_sg.id ]
+    key_name = aws_key_pair.management_key_pair.key_name
+
+    tags = {
+      "Name" = "PEC21"
+    }
+}
+
+resource "aws_instance" "PEC22" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.PrivateSubnet.id
+    associate_public_ip_address = false
+    user_data = local.base_init
+    vpc_security_group_ids = [ aws_security_group.PEC2_sg.id ]
+    key_name = aws_key_pair.management_key_pair.key_name
+
+    tags = {
+      "Name" = "PEC22"
+    }
+}
