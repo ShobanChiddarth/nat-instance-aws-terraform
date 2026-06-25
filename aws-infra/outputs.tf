13a9cb02e625b974d20d38009595968a35a485c9
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-25 09:09:53 +0530

    Added nat_instance private IP to output

--- aws-infra/outputs.tf
+++ aws-infra/outputs.tf
@@ -6,6 +6,10 @@ output "nat_instance_public_ip" {
     value = aws_eip.nat_instance_eip.public_ip
 }
 
+output "nat_instance_private_ip" {
+    value = aws_instance.nat_instance.private_ip
+}
+
 output "PEC21_private_ip" {
     value = aws_instance.PEC21.private_ip
 }
