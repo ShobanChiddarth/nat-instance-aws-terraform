0f9ebc32be33236958f05086fa055f1200209b2a
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-25 09:02:59 +0530

    Added outputs.tf

---
+++ aws-infra/outputs.tf
@@ -0,0 +1,15 @@
+output "bastion_public_ip" {
+    value = aws_instance.bastion.public_ip
+}
+
+output "nat_instance_public_ip" {
+    value = aws_eip.nat_instance_eip.public_ip
+}
+
+output "PEC21_private_ip" {
+    value = aws_instance.PEC21.private_ip
+}
+
+output "PEC22_private_ip" {
+    value = aws_instance.PEC22.public_ip
+}
